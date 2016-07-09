1a7faaefb79145cee78b34bd90e31e8d35c40cad
Evgeniy Kulyk <kulyk.evgeniy@gmail.com>
2016-07-09 19:39:18 +0300

    Interpreter pattern code added

--- 3. [Design]/OOD/Design Patterns (GoF++)/DesignPatterns/Interpreter/Program.cs
+++ 3. [Design]/OOD/Design Patterns (GoF++)/DesignPatterns/Interpreter/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Interpreter
 {
@@ -10,6 +7,47 @@ namespace Interpreter
     {
         static void Main(string[] args)
         {
+            Context context = new Context();
+            var list = new List<AbstractExpression>();
+
+            // Populate 'abstract syntax tree' 
+            list.Add(new TerminalExpression());
+            list.Add(new NonterminalExpression());
+            list.Add(new TerminalExpression());
+            list.Add(new TerminalExpression());
+
+            // Interpret
+            foreach (AbstractExpression exp in list)
+            {
+                exp.Interpret(context);
+            }
+
+            // Wait for user
+            Console.ReadKey();
+        }
+
+        class Context
+        {
+        }
+
+        abstract class AbstractExpression
+        {
+            public abstract void Interpret(Context context);
+        }
+
+        class TerminalExpression : AbstractExpression
+        {
+            public override void Interpret(Context context)
+            {
+                Console.WriteLine("Called Terminal.Interpret()");
+            }
+        }
+        class NonterminalExpression : AbstractExpression
+        {
+            public override void Interpret(Context context)
+            {
+                Console.WriteLine("Called Nonterminal.Interpret()");
+            }
         }
     }
 }
